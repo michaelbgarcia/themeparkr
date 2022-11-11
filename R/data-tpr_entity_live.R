cb07123d82a66dd241da3d1a551b22eb4af16e9a
Michael Garcia <michaelgarcia@michaels-air.lan>
2022-11-10 21:31:47 -0500

    added data snapshots for local use

---
+++ R/data-tpr_entity_live.R
@@ -0,0 +1,9 @@
+#' Themepark Entity Live
+#'
+#' A snapshot of content results from `tpr_entity_live()`
+#'
+#' @format ## `tpr_entity_live_data`
+#' A data frame with 3180 rows and 12 columns
+#'
+#' @source <https://api.themeparks.wiki/docs/v1/#/entities/getEntityLiveData>
+"tpr_entity_live_data"
